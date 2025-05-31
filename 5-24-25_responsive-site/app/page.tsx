5a7536ef1eab265131d6911ab2e95b44135b3e03
Terrance Corley <terrancecorley@outlook.com>
2025-05-30 20:07:14 -0700

    added styles to overlay header menu for mobile

--- 5-24-25_responsive-site/app/page.tsx
+++ 5-24-25_responsive-site/app/page.tsx
@@ -1,13 +1,35 @@
+"use client";
+import { useState } from "react";
+
 export default function Home() {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
+
     return (
         <div className="grid grid-cols-1 grid-rows-[auto, 1fr, auto] h-full">
             <header className="flex justify-between p-4">
                 <h1>Site Logo</h1>
-                <button className="h-5 w-5 flex flex-col justify-between cursor-pointer">
+                <button
+                    className="h-5 w-5 flex flex-col justify-between cursor-pointer"
+                    onClick={() => setIsMobileMenuOpen(true)}
+                >
                     <div className="h-0.5 bg-cyan-500 w-full"></div>
                     <div className="h-0.5 bg-cyan-500 w-full"></div>
                     <div className="h-0.5 bg-cyan-500 w-full"></div>
                 </button>
+                {isMobileMenuOpen && (
+                    <div className="fixed inset-0 z-50 bg-black text-white opacity-70">
+                        <button onClick={() => setIsMobileMenuOpen(false)}>
+                            X
+                        </button>
+                        <ul>
+                            <li>Nav Item 1</li>
+                            <li>Nav Item 2</li>
+                            <li>Nav Item 3</li>
+                            <li>Nav Item 4</li>
+                            <li>Nav Item 5</li>
+                        </ul>
+                    </div>
+                )}
             </header>
             <main>
                 <div className="p-4">
